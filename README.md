d1d19cb456fd014f917ec9d1f7eac3cd3aa49e5e
kallieog <111663090+kallieog@users.noreply.github.com>
2022-12-20 19:12:29 -0600

    Cleaned things up, server still not running

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # M14-MVC
-Module 14 challenge
+
+## Description
+Having fun with MVC in the module 14 challenge. This website is a blog-style website where developers can publish posts and other developers can interact with them.
+
+## User Story
+As a developer, I want to be able to post to a public coding forum, so that I can learn more about coding from other developers.
+
+## Screenshots
+![Screenshot](screenshot1.png)
+
+
---
+++ controllers/homeroutes.js
@@ -0,0 +1,93 @@
+const router = require("express").Router()
+const { Post, User, Comment } = require('../models');
+// const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: { exclude: ['password'] },
+        },
+      ],
+      attributes: [
+        "title", "body", "id", "createdAt"
+      ]
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+   
+    res.render('homepage', { 
+      posts, 
+      logged_in: true 
+    });
+    // res.json(posts)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/posts/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('post', {
+      ...post,
+      logged_in: false
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('signup');
+});
+
+
+module.exports = router
